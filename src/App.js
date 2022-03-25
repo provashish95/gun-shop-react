51988cc00235427e408e135f75c929fca19d55d3
Provashish Roy <provashishroy95@gmail.com>
2022-03-25 14:07:27 +0600

    load data and show data to card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import { useState, useEffect } from 'react';
 import './App.css';
+import Card from './components/Card/Card';
 import Navbar from './components/Navbar/Navbar';
 
 function App() {
+
+  const [guns, setGuns] = useState([]);
+  useEffect(() => {
+    fetch("https://raw.githubusercontent.com/mir-hussain/guns/main/data.json")
+      .then(res => res.json())
+      .then(data => setGuns(data))
+  }, [])
+
+  console.log(guns);
+
   return (
     <div className="App">
       <Navbar></Navbar>
+
+      {
+        guns.map(gun => (
+          <Card key={gun.id} gun={gun}></Card>
+        ))
+      }
     </div>
   );
 }
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
-const Card = () => {
+const Card = (props) => {
+    const { name, price, img, category, capacity, bullet, action } = props.gun;
     return (
         <div>
-
+            <img src={img} alt="img" style={{ width: '200px' }} />
+            <h1>{name}</h1>
+            <p>Price: {price}</p>
+            <p>Category: {category}</p>
+            <p>Capacity: {capacity}</p>
+            <p>Bullet: {bullet}</p>
+            <p>Action: {action}</p>
         </div>
     );
 };
