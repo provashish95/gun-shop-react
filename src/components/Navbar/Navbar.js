1664451f6ecc98ae6c23c00823f4f66fd180ada5
Provashish Roy <provashishroy95@gmail.com>
2022-03-25 12:31:52 +0600

    added component navbar and card

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navbar.css'
+    ;
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>Gun Shop</h1>
+        </nav>
+    );
+};
+
+export default Navbar;
