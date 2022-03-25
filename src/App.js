03a7344280b36f0ea3a193e82a817c934393eaad
Provashish Roy <provashishroy95@gmail.com>
2022-03-25 17:07:12 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -2,12 +2,31 @@ import { useState, useEffect } from 'react';
 import './App.css';
 import Card from './components/Card/Card';
 import Navbar from './components/Navbar/Navbar';
-
+import Modal from 'react-modal';
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
+Modal.setAppElement('#root');
 function App() {
+  const [modalIsOpen, setIsOpen] = useState(false);
 
   const [guns, setGuns] = useState([]);
   const [cart, setCart] = useState([]);
-  console.log(cart);
+  //console.log(cart);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+  function closeModal() {
+    setIsOpen(false);
+  }
 
   useEffect(() => {
     fetch("https://raw.githubusercontent.com/mir-hussain/guns/main/data.json")
@@ -18,18 +37,15 @@ function App() {
   //console.log(guns);
 
   const handleAddToCart = (gun) => {
-    const newGun = [...cart, gun];
+    const newGun = [...cart, gun];  //old array copy and add new array item in the array
     setCart(newGun);
   }
 
   return (
     <div className="App">
-      <Navbar></Navbar>
-      <div>
-        {
-          cart.map(gun => <h1 key={gun.id}>{gun.name}</h1>)
-        }
-      </div>
+      <Navbar openModal={openModal} cart={cart}></Navbar>
+
+
       <div className="card-container">
         {
           guns && guns.map(gun => (
@@ -37,6 +53,20 @@ function App() {
           ))
         }
       </div>
+
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal">
+        <button onClick={closeModal}>Close</button>
+        <div>
+          {
+            cart.map(gun => <h1 key={gun.id}>{gun.name}</h1>)
+          }
+        </div>
+      </Modal>
+
     </div>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import './Navbar.css'
-    ;
-const Navbar = () => {
+import './Navbar.css';
+import { BsFillCartFill } from 'react-icons/bs';
+;
+const Navbar = ({ openModal, cart }) => {
+
     return (
         <nav className="navbar">
             <h1>Gun Shop</h1>
+            <div className='cart-counter' onClick={openModal}>
+                <span>{cart.length}</span>
+                <BsFillCartFill className="icon" color="#fff" />
+            </div>
+
         </nav>
     );
 };
